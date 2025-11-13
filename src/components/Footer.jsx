1105588ffc63d175fab786f6304e99c43b18e8df
Jao <jaohallenbanados@gmail.com>
2025-11-13 15:38:29 +0800

    Adjusted footer thickness, and font size

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,7 +3,7 @@ import React from "react";
 
 const Footer = () => {
   return (
-    <footer className="bg-[#344e41] text-white py-4 text-center text-sm">
+    <footer className="bg-[#344e41] text-white py-2 text-center text-xs">
       © 2025 CENVI | Center for Environmental Informatics
     </footer>
   );
